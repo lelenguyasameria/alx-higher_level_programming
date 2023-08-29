3e0589c20275f5e71addd9690e1da8f9652169b8
lelenguyasameria <somearia711@gmail.com>
2023-08-29 02:48:34 -0700

    classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 '''1-square.py: Python script that defines square,
-private instantiation attribute of size'''
-class Square:
-'''Creates  Square type'''
+   private instantiation attribute of size'''
 
-def __init__(self, size):
-'''Initializes Square with size'''
-self.__size = size
+
+class Square:
+        '''Creates  Square type'''
+
+        def __init__(self, size):
+            '''Initializes Square with size'''
+
+            self.__size = size
+
