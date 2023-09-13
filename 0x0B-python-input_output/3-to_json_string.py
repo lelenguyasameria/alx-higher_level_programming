4a70e5553ba9ef81bf8e100dd66097272c8429ec
lelenguyasameria <somearia711@gmail.com>
2023-09-12 22:45:43 -0700

    inputoutput

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """
-    Python function that returns the JSON representation of an object
+This is a Python function that returns the JSON representation of an object
 """
 
 import json
-
-
 def to_json_string(my_obj):
     return json.dumps(my_obj)
-
