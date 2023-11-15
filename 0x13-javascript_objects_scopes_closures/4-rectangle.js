e1878c6dfc72fda2b3a4d718867a1677067ec0ae
lelenguyasameria <somearia711@gmail.com>
2023-11-14 17:45:35 -0800

    corrected

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,52 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            return {};
+        }
+
+        this.width = w;
+        this.height = h;
     }
-  }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
+    print() {
+        if (!this.width || !this.height) {
+            console.log("Invalid rectangle");
+            return;
+        }
+
+        for (let i = 0; i < this.height; i++) {
+            console.log("X".repeat(this.width));
+        }
     }
-  }
 
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
+    rotate() {
+        if (!this.width || !this.height) {
+            console.log("Invalid rectangle");
+            return;
+        }
 
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
+        [this.width, this.height] = [this.height, this.width];
+    }
+
+    double() {
+        if (!this.width || !this.height) {
+            console.log("Invalid rectangle");
+            return;
+        }
+
+        this.width *= 2;
+        this.height *= 2;
+    }
 }
+
+const rectangle1 = new Rectangle(4, 3);
+rectangle1.print();
+rectangle1.rotate();
+rectangle1.print();
+rectangle1.double();
+rectangle1.print();
+
+const invalidRectangle = new Rectangle(0, 5);
+invalidRectangle.print();
 
