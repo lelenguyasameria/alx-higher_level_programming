629fc5cc6bb8137b2dce8ffc70989cbc28a46741
lelenguyasameria <somearia711@gmail.com>
2023-12-01 06:30:32 -0800

    crazyauthentics

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """
 This script fetches https://alx-intranet.hbtn.io/status using urllib.
-It includes a token in the request headers and displays the body of the response in a specific format.
+It displays the body of the response in a specific format.
 """
 
 import urllib.request as ur
 
-url = 'https://intranet.hbtn.io/status'
-token = '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'
-headers = {'Authorization': f'token {token}', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'}
+url = 'https://alx-intranet.hbtn.io/status'
 
-req = ur.Request(url, headers=headers)
-
-with ur.urlopen(req) as res:
-    res = res.read()
-    print('Body response:$')
-    print('\t- type: {}$'.format(type(res)))
-    print('\t- content: {}$'.format(res))
-    print('\t- utf8 content: {}$'.format(str(res, 'utf-8')))
+with ur.urlopen(url) as response:
+    body = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body.decode('utf-8')))
 
