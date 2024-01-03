04a514fc70ca2284dc7d9eac9e56373c1211d213
lelenguyasameria <somearia711@gmail.com>
2024-01-03 21:22:25 +0300

    mewrite

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* eslint-disable semi */
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
